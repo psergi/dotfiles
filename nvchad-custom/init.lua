7447105af9ddde39c22756202b45a95369a7a973
Phil Sergi <phil.sergi@gmail.com>
2023-08-15 09:53:24 -0700

    Jbuilder seen as ruby file

--- nvchad-custom/init.lua
+++ nvchad-custom/init.lua
@@ -13,6 +13,8 @@ vim.cmd("autocmd Filetype go setlocal noexpandtab shiftwidth=4 tabstop=4")
 vim.cmd("autocmd FileType * if &diff | setlocal ft=diff | endif")
 -- fix for ruby indent issue: https://github.com/nvim-treesitter/nvim-treesitter/issues/3363
 vim.cmd("autocmd FileType ruby setlocal indentkeys-=.")
+-- set jbuilder files as ruby
+vim.cmd("autocmd BufNewFile,BufRead *.json.jbuilder set ft=ruby")
 -- Disable smart index for ruby
 -- vim.cmd("autocmd FileType ruby setlocal smartindent")
 -- fix for Shift-Space outputting ;2u in terminal mode: https://github.com/onivim/oni/issues/2342
