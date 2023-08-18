d94adc8f28b818f0becdc9a9876835fde4d4bf78
Phil Sergi <phil.sergi@gmail.com>
2023-08-18 15:27:17 -0700

    Rely on homebrew to install xcode command line tools

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 mkdir -p ~/Projects/dotfiles
 cd ~/Projects/dotfiles
 git clone https://github.com/psergi/dotfiles.git .
--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Prompt to install Xcode command line tools, ignore if already installed.
-xcode-select --install &> /dev/null
-
 if [[ ! -f ~/.gitconfig_local ]]; then
   echo
   echo "Git user configuration (this will be saved in ~/.gitconfig_local)"
