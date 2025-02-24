50c848728beb60b2748363625d61053f2b9fe6df
Phil Sergi <phil.sergi@gmail.com>
2025-02-23 23:05:11 -0800

    Updated linter config for ALE

--- nvim/lua/plugins/ale.lua
+++ nvim/lua/plugins/ale.lua
@@ -3,8 +3,15 @@ return {
   lazy = false,
   init = function()
     vim.cmd [[
-      let g:ale_linter_aliases = { "vue": ["vue", "javascript"] }
-      let g:ale_linters = { "javascript": ["eslint"], "typescript": ["eslint", "tsserver"], "ruby": ["rubocop"], "vue": ["eslint", "vls"], "go": ["golangci-lint"], "scss": ["stylelint"] }
+      let g:ale_linters = {
+        \ "javascript": ["eslint"],
+        \ "typescript": ["eslint"],
+        \ "typescriptreact": ["eslint"],
+        \ "ruby": ["rubocop"],
+        \ "vue": ["eslint", "vls"],
+        \ "go": ["golangci-lint"],
+        \ "scss": ["stylelint"]
+      \ }
       let g:ale_linters_explicit = 1
       let g:ale_use_neovim_diagnostics_api = 0
       let g:ale_ruby_rubocop_executable = "bundle"
@@ -12,10 +19,6 @@ return {
       if filereadable("bin/docker-cmd")
         let g:ale_command_wrapper = "bin/docker-cmd"
         let g:ale_ruby_rubocop_executable = "rubocop"
-        let g:ale_javascript_eslint_executable = "node_modules/.bin/eslint"
-        let g:ale_javascript_eslint_use_global = 1
-        let g:ale_css_stylelint_executable = "node_modules/.bin/stylelint"
-        let g:ale_css_stylelint_use_global = 1
       endif
       let g:ale_fixers = { "go": ["gofumpt"], "javascript": ["eslint"] }
       let g:ale_fix_on_save = 1
