55794caf633cd07df91754253ed94545f22fbd4b
Phil Sergi <phil.sergi@gmail.com>
2025-10-11 00:33:21 -0700

    Include CMD+ keymaps for nvim

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -11,6 +11,9 @@ vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", default_opts)
 vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", default_opts)
 vim.keymap.set("n", "<C-l>", ":Telescope live_grep_args<CR>", default_opts)
 vim.keymap.set("n", "<C-o>", ":Telescope buffers<CR>", default_opts)
+vim.keymap.set("n", "<D-p>", ":Telescope find_files<CR>", default_opts)
+vim.keymap.set("n", "<D-l>", ":Telescope live_grep_args<CR>", default_opts)
+vim.keymap.set("n", "<D-o>", ":Telescope buffers<CR>", default_opts)
 
 -- Vim Test
 vim.keymap.set("n", "<leader>s", ":TestNearest<CR>", default_opts)
